6dfe0b960ec04c2fccc470c376d6f4fc26bb8ad1
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-04-05 21:56:34 +0000

    App composition with async function to fetch data

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+import InformationCard from './components/InformationCard.vue';
+import UserProfileCard from './components/UserProfileCard.vue';
+
+const userData = ref([]);
+const selectedTimeFrame = ref("daily")
+
+const fetchUserData = async () => {
+  try {
+    const response = await fetch("/db/data.json");
+    const jsonData = await response.json()
+    userData.value = jsonData
+  } catch (error) {
+    console.error("Error loading data: ", error)
+  }
+}
+
+onMounted(() => fetchUserData())
+
+const getClickedValue = (s) => {
+  selectedTimeFrame.value = s
+}
 
 </script>
 
 <template>
-  <h1>
-    Hello World
-  </h1>
+  <UserProfileCard @handleClick="getClickedValue" />
+  <InformationCard v-for="{title, timeframes},index in userData" :card-title="title"
+    :current-time-value="timeframes[selectedTimeFrame].current"
+    :previous-time-value="timeframes[selectedTimeFrame].previous" :key="index" />
 
 </template>
