c67077523a1e7014c28c64ed911d5de27c4a57b5
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-04-05 21:54:49 +0000

    Card component that receives data values from App

---
+++ src/components/InformationCard.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+const props = defineProps({
+    cardTitle: {
+        type: String,
+        required: true
+    },
+    currentTimeValue: {
+        type: Number,
+        required: true
+    },
+    previousTimeValue: {
+        type: Number,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <section class="information--card">
+        <div class="information--details">
+            <h2>
+                {{ cardTitle }}
+                <span>...</span>
+            </h2>
+        </div>
+        <div class="card--details">
+            <p class="current--value">{{ currentTimeValue }}</p>
+            <p class="previous--value">Last week - {{ previousTimeValue }}</p>
+        </div>
+    </section>
+</template>
