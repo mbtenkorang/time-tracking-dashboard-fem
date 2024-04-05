be1bd07e7b43c5fd623d1f5007e7fd133228dea6
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-04-05 21:55:35 +0000

    User profile component with nav links to view corresponding data

---
+++ src/components/UserProfileCard.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+const emit = defineEmits(['handleClick'])
+
+const handleClick = (event) => {
+    emit('handleClick', event.target.innerHTML.toLowerCase())
+}
+</script>
+
+<template>
+    <div>
+        <header class="card">
+            <div>
+                <img src="" alt="">
+                <h1>
+                    <span>Report for </span>
+                    <span>Jeremy Robson</span>
+                </h1>
+            </div>
+        </header>
+        <nav>
+            <a @click.prevent="handleClick" href="daily">Daily</a>
+            <a @click.prevent="handleClick" href="weekly">Weekly</a>
+            <a @click.prevent="handleClick" href="monthly">Monthly</a>
+        </nav>
+    </div>
+
+</template>
