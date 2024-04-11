c60bff4ca74f4b6779379df46cbd71981232441b
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-04-11 10:50:11 +0000

    Added reactive elements for hr/hrs and time labels

--- src/components/InformationCard.vue
+++ src/components/InformationCard.vue
@@ -12,21 +12,40 @@ const props = defineProps({
     previousTimeValue: {
         type: Number,
         required: true
+    },
+    selectedTime: {
+        type: String
     }
 })
+
 </script>
 
 <template>
-    <section class="information--card">
-        <div class="information--details">
-            <h2>
-                {{ cardTitle }}
+    <section class="information--container" :class="cardTitle.toLowerCase()">
+        <div class="information--card">
+            <div class="information--title">
+                <h2>
+                    {{ cardTitle }}
+                </h2>
                 <span>...</span>
-            </h2>
+            </div>
+            <div class="card--details">
+                <p class="current--value">{{ currentTimeValue }}
+                    {{ currentTimeValue > 1 || currentTimeValue === 0 ?
+                    'hrs' :
+                    'hr' }}
+                </p>
+                <p v-if="selectedTime==='daily'" class="previous--value">Yesterday - {{ previousTimeValue }}
+                    {{previousTimeValue > 1 || previousTimeValue === 0 ? "hrs" : 'hr' }}
+                </p>
+                <p v-else-if="selectedTime === 'weekly'" class="previous--value">Last Week - {{ previousTimeValue}}
+                    {{previousTimeValue > 1 || previousTimeValue === 0 ? "hrs" : 'hr' }}
+                </p>
+                <p v-else class="previous--value">Last Month - {{ previousTimeValue }}
+                    {{previousTimeValue > 1 || previousTimeValue === 0 ? "hrs" : 'hr' }}
+                </p>
+            </div>
         </div>
-        <div class="card--details">
-            <p class="current--value">{{ currentTimeValue }}</p>
-            <p class="previous--value">Last week - {{ previousTimeValue }}</p>
-        </div>
+
     </section>
 </template>
