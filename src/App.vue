5fed89c5450e781e4c1fb849033632882c798ac0
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-04-13 20:41:51 +0000

    Chnaged name of emit event function

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ const selectedValue = (s) => {
 </script>
 
 <template>
-  <UserProfileCard @gettime="selectedValue" />
+  <UserProfileCard @getrecordforperiod="selectedValue" />
   <InformationCard v-for="{title, timeframes},index in userData" :card-title="title" :selected-time="selectedTimeFrame"
     :current-time-value="timeframes[selectedTimeFrame].current"
     :previous-time-value="timeframes[selectedTimeFrame].previous" :key="index" />
